42b7aff6eebc0919e61f08fe01adf6b1aca810ff
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-09-06 01:31:30 +0300

    added authenticatoion

--- projects/admin/src/app/app.component.ts
+++ projects/admin/src/app/app.component.ts
@@ -1,43 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../../libs/src/firebase.service';
-import { map, finalize } from "rxjs/operators";
-import { Observable } from "rxjs";
+import { map, finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent implements OnInit {
-  title = "cloudsSorage";
+  title = 'cloudsSorage';
   selectedFile: File = null as any;
   fb: any;
   downloadURL!: Observable<string>;
-  guard!:boolean;
-  constructor(private firebaseService: FirebaseService,private storage: AngularFireStorage) {
-
-  }
+  guard!: boolean;
+  constructor(
+    private firebaseService: FirebaseService,
+    private storage: AngularFireStorage
+  ) {}
 
   isCollapsed = false;
 
   ngOnInit(): void {
-    
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.firebaseService.requests.subscribe(res => {
+    this.firebaseService.requests.subscribe((res) => {
       // console.log('requests', res);
-      
-    })
-    this.firebaseService.startups.subscribe(res => {
+    });
+    this.firebaseService.startups.subscribe((res) => {
       // console.log('startups', res);
-    })
+    });
 
-    this.firebaseService.sectors.subscribe(res =>{
-        //  console.log('sectors', res);
-    })
+    this.firebaseService.sectors.subscribe((res) => {
+      //  console.log('sectors', res);
+    });
 
-  //   this.firebaseService.addStartup(startup)
+    //   this.firebaseService.addStartup(startup)
   }
   // onFileSelected(event:any) {
   //   let n = Date.now() + ".jpg";
@@ -64,5 +63,4 @@ export class AppComponent implements OnInit {
   //       }
   //     });
   // }
-
 }
