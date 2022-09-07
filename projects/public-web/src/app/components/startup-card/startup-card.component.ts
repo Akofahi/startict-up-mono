1b8f64d15262f59d0eff68ba39ae34c6f05f19e5
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-09-08 01:13:15 +0300

    public web startup list design

---
+++ projects/public-web/src/app/components/startup-card/startup-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-startup-card',
+    templateUrl: './startup-card.component.html',
+    styleUrls: ['./startup-card.component.scss']
+})
+export class StartupCardComponent implements OnInit {
+    focus;
+    constructor() {
+    }
+
+    ngOnInit() {
+    
+    }
+}
